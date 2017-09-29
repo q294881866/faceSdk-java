71f7339510fd27fc376e49df1ee2bad71239be01
shoubin <shoubin@iscas.ac.cn>
2017-09-29 15:42:44 +0800

    delete old sdk jni

--- java/jni/build.sh
+++
@@ -1,6 +0,0 @@
-echo start compile demo bin file 
-/usr/bin/g++ com_qs_face_QsFace.cpp -shared -fPIC -o libwisfacejni.so \
--I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" \
--L/root/wisfacesdk/app -Wl,-rpath=/root/wisfacesdk/app -lwisface \
--L/usr/lib/x86_64-linux-gnu/ -Wl,-rpath=/usr/lib/x86_64-linux-gnu -lopencv_highgui -lopencv_imgproc -lopencv_core -lopencv_objdetect \
--Wl,-rpath,/usr/local/lib -fopenmp -L./ -Wl,-rpath=./
--- java/jni/build_JNIFaceBase.sh
+++
@@ -1,4 +0,0 @@
-echo start compile demo bin file 
-/usr/bin/g++ com_face_base.cpp -shared -fPIC -o libjnifaceentity.so \
--I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" \
-
